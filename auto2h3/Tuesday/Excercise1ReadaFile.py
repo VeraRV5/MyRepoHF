295909544221e153d6f51fdaf86d17580021a010
Kerveen Roy Juancito Vergara (KERV0007) <kerv0007@zbc.dk>
2026-01-06 10:16:13 +0100

    Excercise 1 - Read a file -Update 2
    Made Path module to work, and able to open sample.txt file using open() & read() functions
    -Roy

--- auto2h3/Tuesday/Excercise1ReadaFile.py
+++ auto2h3/Tuesday/Excercise1ReadaFile.py
@@ -1,7 +1,14 @@
 from pathlib import Path
 
 
-#using Path, to locate the currect parent directory
+#using Path, to locate the currect parent directory, where script is placed together with the sample.txt file
 BasePath = Path(__file__).parent
 
-sampleLoc = BasePath
+#Combining Base path with the file, to complete a full path to the file
+sampleLoc = BasePath / 'sample.txt'
+
+#open the file using open()
+File = sampleLoc.open()
+
+#Reading File using read()
+print(File.read())
