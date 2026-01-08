829c14ceb9f8c7cd24433ad321d61879f6e67d53
Kerveen Roy Juancito Vergara (KERV0007) <kerv0007@zbc.dk>
2026-01-08 12:28:40 +0100

    Get-Request - Update 1
    Final Code for get-request, made a function, that takes a link and uses try and catch method to request data from the website, and catch error with exception, and printing it out, it prints status code, headers, and body from the website. Combination of Opgave 1 to 3
    -Roy

--- auto2h3/Wed&Thur/Extra-Get-Request.py
+++ auto2h3/Wed&Thur/Extra-Get-Request.py
@@ -1,5 +1,25 @@
 import requests
 
-r = requests.get("https://httpbin.org/get")
-headers = r.headers()
-print (r)
+
+def httpinfo (Link):
+   L = Link
+   r = requests.get(L)
+
+   try:
+      print ("Status Code: ", r.status_code)
+      print ("Headers: ", r.headers)
+      print ("Body:", r.text)
+   except Exception as e:
+      print ("Error:", e)
+
+
+
+
+L = "https://httpbin.org/get"
+httpinfo(L)
+
+H = "https://httpbin.org/headers"
+httpinfo(H)
+
+C = "https://httpbin.org/get?navn=ditnavn&alder=20"
+httpinfo(C)
