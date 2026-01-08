3b91180b3f9ac64131d50db9773b448906b925cd
Kerveen Roy Juancito Vergara (KERV0007) <kerv0007@zbc.dk>
2026-01-08 10:07:11 +0100

    Network Reachability Checker - Update 1
    Added the final code inside a defined function
    -Roy

--- auto2h3/Wed&Thur/Network-Reachability-Checker.py
+++ auto2h3/Wed&Thur/Network-Reachability-Checker.py
@@ -2,26 +2,37 @@ import socket
 
 
 
+def ServerChecker (Host, Port):
 #Make a simple socket, that handle IPV4 with .AF_INET, and use connection TCp with SOCK_STREAM
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-GoogleIP = socket.gethostbyname("gooogle.com")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    GoogleIP = socket.gethostbyname("gooogle.com")
+    H = Host
+    P = Port
 
+#Make a try and catch, try to connect to server, with timeout, and try to send a binary comment, 
+    try:
+      s.settimeout(5)
+      s.connect((H, P))
+      print ("Reachable!")
 
-try:
-    s.settimeout(15)
-    s.connect((GoogleIP, 80))
-    print ("Reachable!")
-
-    s.sendall(b"Hello")
-    Reply = s.recv(1024)
-    print (Reply.decode())
-
-except Exception as e:
-    print ("error occured:", e)
-
-finally:
-    s.close()
+      s.sendall(b"Hello")
+      Reply = s.recv(1024)
+      print (Reply.decode())
+# Make an exception to catch error, if timeoutoccurs, the server is not responding to packet send
+    except socket.timeout:
+      print ("No answer from server")
+# Make an exception for any other errors, and print the error out
+    except Exception as e:
+      print ("Can't reach!!")
+      print ("error occured:", e)
+#Finally close the socket connection
+    finally:
+      s.close()
     
 
 
+
+H = input("Write Hostname or IP: ")
+P = int(input("Write which port to connect to: "))
+ServerChecker(H, P)
 
